192538bb5627eb01f5d4bb0bc563762d757a9a84
Thibault Caron <thibault.caron@laplateforme.io>
2026-05-09 11:48:31 +0200

    fix: reactor integration test no longer expect immediat command after register sent

--- server/tests/reactor_integration_test.cpp
+++ server/tests/reactor_integration_test.cpp
@@ -7,7 +7,6 @@
 #include "helpers_test.hpp"
 #include "poller/epoller.hpp"
 #include "protocol/protocol_parser.hpp"
-#include "protocol/protocol_serializer.hpp"
 #include "reactor.hpp"
 #include "server_dispatcher.hpp"
 #include "socket/socket_test_helpers.hpp"
@@ -36,7 +35,7 @@ void stopReactor(Reactor& reactor, std::uint16_t port) {
 
 }  // namespace
 
-TEST(ReactorIntegration, should_accept_register_and_send_command) {
+TEST(ReactorIntegration, should_accept_register_without_error) {
   TcpServer server(TestConstants::REACTOR_HAPPY_PATH_PORT);
   ASSERT_TRUE(server.start());
 
@@ -55,17 +54,6 @@ TEST(ReactorIntegration, should_accept_register_and_send_command) {
   const auto registerFrame =
       makeRawFrame(MessageType::REGISTER, makeRegisterPayload("reactor-1"));
   ASSERT_TRUE(sendAll(clientFd, registerFrame));
-
-  const std::vector<std::uint8_t> commandHeaderBytes =
-      recvExact(clientFd, LPTF_HEADER_SIZE);
-  ASSERT_EQ(commandHeaderBytes.size(), LPTF_HEADER_SIZE);
-  const LptfHeader commandHeader =
-      ProtocolParser::parseHeader(commandHeaderBytes);
-  EXPECT_EQ(commandHeader.type, MessageType::COMMAND);
-
-  const std::vector<std::uint8_t> commandPayloadBytes =
-      recvExact(clientFd, commandHeader.size);
-  ASSERT_EQ(commandPayloadBytes.size(), commandHeader.size);
 
   ::close(clientFd);
 
@@ -90,13 +78,7 @@ TEST(ReactorIntegration, should_send_error_when_first_message_not_register) {
       connectLoopback(TestConstants::REACTOR_INVALID_FIRST_MESSAGE_PORT);
   ASSERT_NE(clientFd, -1);
 
-  CommandPayload command;
-  command.id = 0;
-  command.type = CommandType::OS_INFO;
-  command.data = "";
-  const std::vector<std::uint8_t> commandPayload =
-      ProtocolSerializer::serializeCommandPayload(command);
-  const auto commandFrame = makeRawFrame(MessageType::COMMAND, commandPayload);
+    const auto commandFrame = makeRawFrame(MessageType::COMMAND);
   ASSERT_TRUE(sendAll(clientFd, commandFrame));
 
   const std::vector<std::uint8_t> errorHeaderBytes =
@@ -136,15 +118,6 @@ TEST(ReactorIntegration, should_remove_disconnected_agent_and_keep_serving) {
       makeRawFrame(MessageType::REGISTER, makeRegisterPayload("reactor-a"));
   ASSERT_TRUE(sendAll(firstClientFd, firstRegister));
 
-  const std::vector<std::uint8_t> commandHeaderBytes =
-      recvExact(firstClientFd, LPTF_HEADER_SIZE);
-  ASSERT_EQ(commandHeaderBytes.size(), LPTF_HEADER_SIZE);
-  const LptfHeader firstCommandHeader =
-      ProtocolParser::parseHeader(commandHeaderBytes);
-  EXPECT_EQ(firstCommandHeader.type, MessageType::COMMAND);
-  const std::vector<std::uint8_t> firstCommandPayloadBytes =
-      recvExact(firstClientFd, firstCommandHeader.size);
-  ASSERT_EQ(firstCommandPayloadBytes.size(), firstCommandHeader.size);
   ::close(firstClientFd);
   std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
@@ -155,16 +128,6 @@ TEST(ReactorIntegration, should_remove_disconnected_agent_and_keep_serving) {
   const auto secondRegister =
       makeRawFrame(MessageType::REGISTER, makeRegisterPayload("reactor-b"));
   ASSERT_TRUE(sendAll(secondClientFd, secondRegister));
-
-  const std::vector<std::uint8_t> secondCommandHeaderBytes =
-      recvExact(secondClientFd, LPTF_HEADER_SIZE);
-  ASSERT_EQ(secondCommandHeaderBytes.size(), LPTF_HEADER_SIZE);
-  const LptfHeader secondCommandHeader =
-      ProtocolParser::parseHeader(secondCommandHeaderBytes);
-  EXPECT_EQ(secondCommandHeader.type, MessageType::COMMAND);
-  const std::vector<std::uint8_t> secondCommandPayloadBytes =
-      recvExact(secondClientFd, secondCommandHeader.size);
-  ASSERT_EQ(secondCommandPayloadBytes.size(), secondCommandHeader.size);
 
   ::close(secondClientFd);
 
