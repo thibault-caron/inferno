4f4b224d3751a2ce59d5c953adc94bf5b9ebfe77
Thibault Caron <tcaronv@proton.me>
2026-08-04 09:37:50 +0200

    feat: windows metric scrapper & system monitor v00

--- agent/include/system_monitor/system_monitor_factory.hpp
+++ agent/include/system_monitor/system_monitor_factory.hpp
@@ -1,18 +1,18 @@
 #ifndef SYSTEM_MONITOR_FACTORY_HPP
 #define SYSTEM_MONITOR_FACTORY_HPP
 
-#include "system_monitor/i_system_monitor.hpp"
 
 #include <memory>
 
 #ifdef _WIN32
-  #include "system_monitor/windows_system_monitor.hpp"
+#include "system_monitor/windows_system_monitor.hpp"
 #elif defined(__linux__)
-  #include "system_monitor/linux_system_monitor.hpp"
+#include "system_monitor/linux_system_monitor.hpp"
 #elif defined(__APPLE__)
-  #include "system_monitor/apple_system_monitor.hpp" // macOS is POSIX-compatible, same impl works
+#include "system_monitor/apple_system_monitor.hpp" // macOS is POSIX-compatible, same impl works
 #endif
 
+#include "system_monitor/i_system_monitor.hpp"
 class SystemMonitorFactory {
 public:
     static std::unique_ptr<ISystemMonitor> createSystemMonitor();
